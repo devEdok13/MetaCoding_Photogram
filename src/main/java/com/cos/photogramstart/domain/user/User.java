d9ead0555214b5c9f6fa49a42f431f05bc4c3e78
devEdok13 <dev.edok13@gmail.com>
2022-08-19 08:34:48 +0900

    220819_L20_Definition-Validation&Exception_Done

--- src/main/java/com/cos/photogramstart/domain/user/User.java
+++ src/main/java/com/cos/photogramstart/domain/user/User.java
@@ -20,7 +20,7 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY) // 번호 증가 전략이 DB를 따라간다 (mysql : auto-increment 전략, oracle : sequence 전략)
   private Long id;
 
-  @Column(unique = true)
+  @Column(unique = true, length = 20)
   private String username;
   private String password;
   private String name;
